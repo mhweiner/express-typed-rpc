4f047deeae9481369f22c7e344d5424b92f7fc8c
Marc H. Weiner <mhweiner234@gmail.com>
2023-05-30 12:49:53 -0400

    fix: remove unnecessary dep

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
-export * from './parseBodyJson';
 export * from './server';
--- src/parseBodyJson.ts
+++
@@ -1,4 +0,0 @@
-import * as bpmod from 'body-parser';
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const parseBodyJson = () => bpmod.json();
